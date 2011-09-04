342847829f0661c70357d85c340c2f9025977381
Marshall Sontag <marshallsontag@gmail.com>
2011-09-04 12:55:23 -0700

    insert_after deprecated. Replaced with Deface::Override.new

--- lib/spree_subscriptions_hooks.rb
+++ lib/spree_subscriptions_hooks.rb
@@ -1,5 +1,9 @@
 class SpreeSubscriptionsHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_product_form_right, "shared/ext_subscription_admin_product_fields"
+  Deface::Override.new(:virtual_path => "admin/products/_form",
+                       :name => "converted_admin_product_form_right_494711790",
+                       :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                       :partial => "shared/ext_subscription_admin_product_fields",
+                       :disabled => false)
   
   #insert_after :admin_tabs do
   #  %(<%= tab(:subscriptions)  %>)
