434fa333f1b6c3356cd25988b3d8eed749832de3
Marshall Sontag <marshallsontag@gmail.com>
2011-09-05 00:32:46 -0700

    Moved the Deface override to a dedicated folder, as recommended by spree docs.

---
+++ app/overrides/converted_admin_product_form_right.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "admin/products/_form",
+                     :name => "converted_admin_product_form_right_494711790",
+                     :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                     :partial => "shared/ext_subscription_admin_product_fields",
+                     :disabled => false)
